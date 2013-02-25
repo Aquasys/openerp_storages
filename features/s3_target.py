7c2492f36b4e26956d300a093c51a94d2620f34c
Hinesh Rathod <hineshr3@gmail.com>
2013-02-24 21:54:58 -0800

    modifies test case for s3 storage

--- features/s3_target.py
+++ features/s3_target.py
@@ -1,18 +1,18 @@
 from lettuce import *
 import boto
 
-@step('Store connection information to external storage s3')
-def store_connection_informations(step, url, bucket, AWS_ACCESS_KEY_ID, AWS_ACCESS_KEY_ID):
+@step('When I click on "save"')
+def store_connection_informations(step, url, bucket, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
 	world.url = url
 	world.bucket = bucket
 	world.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
-	world.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
+	world.AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
 
 @step('I click on "test connection"')
 def test_connection(step):
 	try:
 		conn = boto.connect_s3(world.AWS_ACCESS_KEY_ID, world.AWS_ACCESS_KEY_ID)
 		conn.get_bucket(world.bucket)
-	Exception e:
+	except e:
 		return -1
 	return 1
