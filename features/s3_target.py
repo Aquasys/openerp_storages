a8468fd6ccf2b9c0a3cbae17fd61fb8fa1a779df
Hinesh Rathod <hineshr3@gmail.com>
2013-02-25 20:29:36 -0800

    test case file changed according to lettuce_terrain

--- features/s3_target.py
+++ features/s3_target.py
@@ -1,18 +1,10 @@
 from lettuce import *
+from lettuceterrain.terrain import *
 import boto
 
-@step('When I click on "save"')
+@step('When I click on button "save"')
 def store_connection_informations(step, url, bucket, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
-	world.url = url
-	world.bucket = bucket
-	world.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
-	world.AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
-
-@step('I click on "test connection"')
-def test_connection(step):
-	try:
-		conn = boto.connect_s3(world.AWS_ACCESS_KEY_ID, world.AWS_ACCESS_KEY_ID)
-		conn.get_bucket(world.bucket)
-	except e:
-		return -1
-	return 1
+    world.url = url
+    world.bucket = bucket
+    world.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
+    world.AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
