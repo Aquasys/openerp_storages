277f96631e41809a6b416ad835ab9e263d935ad1
Adrien Lemaire <lemaire.adrien@gmail.com>
2013-03-12 13:25:21 +0900

    add lettuceterrain in the requirements

--- features/s3_target-steps.py
+++ features/s3_target-steps.py
@@ -1,10 +1,3 @@
-from lettuce import *
-from lettuceterrain.terrain import *
 import boto
-
-@step('When I click on button "save"')
-def store_connection_informations(step, url, bucket, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
-    world.url = url
-    world.bucket = bucket
-    world.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
-    world.AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
+from lettuce.decorators import step
+from lettuceterrain.terrain import *  # NOQA
