8a2e5e63a162ec2bd55d188fc0c9e3aa0eda5491
Hinesh Rathod <hineshr3@gmail.com>
2013-03-20 19:24:35 -0700

    change in test connection method and added doc string

--- external_storage_openerp/s3_interface.py
+++ external_storage_openerp/s3_interface.py
@@ -47,7 +47,12 @@ import base64
 
 def sha_file_naming(filename):
     ''' Encrypted file name using sha algoritham
-    to achive uniqueness of files on AWS S3'''
+    to achive uniqueness of files on AWS S3
+
+    @param: File data (contant)
+
+    @return: Encrypted file name
+    '''
     sha_object = hashlib.sha256(filename)
     return sha_object.hexdigest()
 
@@ -56,7 +61,13 @@ def s3_set_file(cr, obj, id, name,
                 value, user=SUPERUSER_ID, context={}):
     '''Upload file to AWS S3 using boto
     Track log in lookup table for mapping of external
-    file name to openerp record'''
+    file name to openerp record
+
+    @param: DB Cusrsor, Object Pool, Resource ID, Binary Field Name,
+            binary data to store, User ID
+
+    @return: Encrypt file name, which file store on s3
+    '''
     encrypt_filename = ''
     cr.execute('select company_id from res_users where id = %s' %
                (user))
@@ -99,7 +110,12 @@ def s3_set_file(cr, obj, id, name,
 
 
 def s3_get_file(cr, obj, i, name, user=SUPERUSER_ID, context={}, values=[]):
-    ''' Download file from AWS S3 '''
+    ''' Download file from AWS S3
+
+    @param: DB Cusrsor, Object Pool, Resource ID, Binary Field Name, User ID
+
+    @return: binary data
+    '''
     data = ''
     cr.execute('select company_id from res_users where id = %s' %
                (user))
@@ -130,6 +146,14 @@ def s3_get_file(cr, obj, i, name, user=SUPERUSER_ID, context={}, values=[]):
     return data
 
 def connection_test(cr, obj, id, name, user=SUPERUSER_ID, context={}):
+    '''
+    To check connection and verify for external storage module is 
+    installed or not
+
+    @params: DB Cusrsor, Object Pool, Resource ID, Binary Field Name, User ID
+
+    @return: True or False
+    '''
     s3_installed = False
     s3_connection_info = False
     bucket = False
@@ -138,9 +162,10 @@ def connection_test(cr, obj, id, name, user=SUPERUSER_ID, context={}):
         s3_installed = cr.fetchall()
     except Exception as detail:
         logging.error(detail)
+    if not s3_installed:
+        return False
     try:
-        cr.execute('select company_id from res_users where id = %s' %
-                   (user))
+        cr.execute('select company_id from res_users where id = %s' %(user))
         company_id = cr.fetchall()
         company_id = tools.misc.flatten(company_id)
         cr.execute('select aws_access_key_id,aws_secret_access_key,bucket from\
@@ -148,13 +173,13 @@ def connection_test(cr, obj, id, name, user=SUPERUSER_ID, context={}):
         s3_connection_info = tools.misc.flatten(cr.fetchall())
     except Exception as detail:
         logging.error(detail)
+    if not s3_connection_info:
+        return False
     try:
         s3 = boto.connect_s3(s3_connection_info[0], s3_connection_info[1])
         bucket = s3.get_bucket(s3_connection_info[2])
     except Exception as detail:
         logging.error(detail)
-
-    if s3_installed and s3_connection_info and bucket:
-        return True
-    else:
+    if not bucket:
         return False
+    return True
