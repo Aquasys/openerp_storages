4af8953462a3d4bb7ecc41ef15650e2784acd6b2
Joyal Shah <jacksj.2325@gmail.com>
2013-09-03 17:24:52 +0800

    add docstring

--- external_storage_openerp/lookup/lookup.py
+++ external_storage_openerp/lookup/lookup.py
@@ -48,29 +48,38 @@ class lookup(osv.osv):
                 'field_name': fields.char('Field Name', size=256),
                 }
 
-    #Unlink method to delte the records from Openerp S3 lookup table 
+    #Unlink method to delete the records from Openerp S3 lookup instance
     #and delteing respective records with same file_name from AWS S3 Bucket
-    def unlink(self, cr, uid, ids, context=None):        
+    def unlink(self, cr, uid, ids, context=None):
+        """
+        Unlink Method to delete the record from lookup table and also deleting
+        respecive records with same name from AWS S3 bucket
+        @param cr: DB Cusrsor
+        @param uid: current user id
+        @param ids: ids of records we want to delete
+        @return: osv.osv.unlink method; Responsible to delete records
+                 from lookup instance
+        """
         #Connecting to AWS S3 Object
         user_obj = self.pool.get('res.users')
         company_id = user_obj.read(cr, uid, uid, ['company_id'],\
-                            context=context)['company_id']        
+                               context=context)['company_id']
         s3_connection_info = self.pool.get('res.company').read(cr, uid, \
                             [company_id[0]], ['aws_access_key_id',\
-                            'aws_secret_access_key','bucket'])[0]        
+                            'aws_secret_access_key','bucket'])[0]
         try:
             s3 = boto.connect_s3(s3_connection_info['aws_access_key_id'],\
                          s3_connection_info['aws_secret_access_key'])
-            bucket = s3.get_bucket(s3_connection_info['bucket'])                
+            bucket = s3.get_bucket(s3_connection_info['bucket'])
             logging.info("Connection successful to AWS S3")
-            k = Key(bucket)            
-        except Exception as detail:                
+            k = Key(bucket)
+        except Exception as detail:
             logging.error(detail)
-        for r in self.read(cr, uid, ids, ['en_file_name']):      
-             #Check if OpenERP S3 Lookup filename exist in bucket       
+        for r in self.read(cr, uid, ids, ['en_file_name']):
+             #Check if OpenERP S3 Lookup filename exist in bucket
              if bucket.get_key(r['en_file_name']):
-                k.key = r['en_file_name']                 
-                bucket.delete_key(k)       
-        return osv.osv.unlink(self, cr, uid, ids, context=context)
+                k.key = r['en_file_name']
+                bucket.delete_key(k)
+        return super(lookup, self).unlink(self, cr, uid, ids, context=context)
 lookup()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
